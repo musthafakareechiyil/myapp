5af2f8464e6aa299e7d6bad8983d7dc1f4dcfd19
musthafa <musthafakareechiyil@gmail.com>
2023-06-10 19:44:05 +0530

    myapp

--- src/index.js
+++ src/index.js
@@ -4,12 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const heading = React.createElement(
+  "h1",
+  {
+      id:"title",
+  },
+  "heading 1"
 );
+const heading2 = React.createElement(
+  "h2",
+  {
+      id:"title",
+  },
+  "heading 2"
+);
+const heading3 = React.createElement(
+  "h3",
+  {
+      id:"title",
+  },
+  "heading 3"
+);
+const container = React.createElement(
+  "div",
+  {
+  id:"container"
+  },
+  [heading, heading2, heading3]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
