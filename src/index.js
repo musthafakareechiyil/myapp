cb8af5278954e436bc120c050e42034394f2cc13
musthafa <musthafakareechiyil@gmail.com>
2023-06-12 20:03:52 +0530

    react basics clear

--- src/index.js
+++ src/index.js
@@ -4,24 +4,106 @@ import "./index.css";
 // import App from './App' ;
 import reportWebVitals from "./reportWebVitals";
 
-const Heading = ()=>(
-	<div>
-		<h1 id="title" key="h1">
-			heading 1
-		</h1>
+const Title = () => (
+  <a href="/">
+    <div>
+      <img class= "logo" alt="logo" src="https://foodvillages.com/img/icon-resized.jpg"></img>
+    </div>
+  </a>
+);
+
+const Header = () => (
+	<div className="header">
+		<Title />
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
 	</div>
 );
 
-const HeaderComponent = () => (
-  <div>
-    <Heading/>
-    <h1>Hello world functional component</h1>
-    <h2>this is h2 tag</h2>
-  </div>
-);
+const config = [
+  {
+    type: "carousel",
+    cards: [
+      {
+        offerName: "50% off"
+      },
+      {
+        offerName: "no delivery charge"
+      }
+    ]
+  },
+  {
+    type: "restaurants",
+    cards: [
+      {
+        offerName: "50% off"
+      },
+      {
+        offerName: "no delivery charge"
+      }
+    ]
+  }
+]
+
+const reataurantList = {
+  name: "KFC",
+  image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426",
+  cusines: ["Burgers ", "Biriyani", "American"],
+  rating: "4.2"
+}
+
+const RestraundCard = () => {
+  return (
+    <div className="card">
+      <img src={KFC.image} alt=""/>
+      <h2>{KFC.name}</h2>
+      <h3>{KFC.cusines.join(", ")}</h3>
+      <h4>{KFC.rating} stars</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+      <RestraundCard/> 
+
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <h4>Footer</h4>
+  )
+}
+
+const Applayout = () => {
+  return (
+    <><Header /><Body /><Footer /></>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent/>);
+root.render(<Applayout />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
